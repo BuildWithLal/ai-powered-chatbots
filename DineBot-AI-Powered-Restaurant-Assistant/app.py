ec1e42de2c735dc9c40b7004ac1b917b05a2df04
Lal <lalzadamohmand@gmail.com>
2024-10-02 22:38:21 +0500

    add Google and Github logins

--- DineBot-AI-Powered-Restaurant-Assistant/app.py
+++ DineBot-AI-Powered-Restaurant-Assistant/app.py
@@ -1,5 +1,6 @@
 import chainlit as cl
 from src.llm import take_order
+from typing import Dict, Optional
 
 
 @cl.on_chat_start
@@ -35,6 +36,31 @@ async def set_starters():
             )
         ]
 
+
+@cl.password_auth_callback
+def auth():
+    return cl.User(identifier="test")
+
+@cl.password_auth_callback
+def auth_callback(username: str, password: str):
+    # Fetch the user matching username from your database
+    # and compare the hashed password with the value stored in the database
+    if (username, password) == ("admin", "admin"):
+        return cl.User(
+            identifier="admin", metadata={"role": "admin", "provider": "credentials"}
+        )
+    else:
+        return None
+    
+
+@cl.oauth_callback
+def oauth_callback(
+  provider_id: str,
+  token: str,
+  raw_user_data: Dict[str, str],
+  default_user: cl.User,
+) -> Optional[cl.User]:
+  return default_user
 
 @cl.on_message
 async def main(message: cl.Message):
