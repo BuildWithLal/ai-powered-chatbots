46c28ea6e8f8d0fe6626c00ffbe3f266f7df46fb
Lal <lalzadamohmand@gmail.com>
2024-10-03 20:43:32 +0500

    move starters to json

--- DineBot-AI-Powered-Restaurant-Assistant/app.py
+++ DineBot-AI-Powered-Restaurant-Assistant/app.py
@@ -1,3 +1,5 @@
+import os
+import json
 import chainlit as cl
 from src.llm import take_order
 from typing import Dict, Optional
@@ -10,30 +12,20 @@ def on_chat_start():
 
 @cl.set_starters
 async def set_starters():
-    return [
-        cl.Starter(
-            label="Din Tai Fung",
-            message="Known for its meticulous and delicious dumplings, Din Tai Fung offers a variety of Taiwanese dishes.",
-            icon="/public/din-tai-fung.png",
-            ),
-
-        cl.Starter(
-            label="Narisawa",
-            message="Michelin-starred Narisawa fuses French techniques with traditional Japanese ingredients, focusing on seasonality and environmental sustainability.",
-            icon="/public/narisawa.jpg",
-            ),
-        cl.Starter(
-            label="Gaggan Anand",
-            message="Gaggan Anand is renowned for his innovative twist on traditional Indian cuisine, often presented in a tasting menu format.",
-            icon="/public/gaggan-anand.png",
-            ),
-        cl.Starter(
-            label="Song Fang Zhai",
-            message="A hidden gem known for authentic Chinese cuisine, particularly the Beijing specialties.",
-            icon="/public/song-fang-zhai.jpg",
-            )
-        ]
-
+    restaurants = []
+    with open(os.path.join('public', 'restaurants.json'), 'r') as f:
+        restaurants = json.loads(f.read())
+    
+    starter_restaurants = []
+    for restaurant in restaurants:
+        starter_restaurants.append(
+                cl.Starter(
+                label=restaurant['label'],
+                message=restaurant['message'],
+                icon=restaurant['icon'],
+            ))
+    
+    return starter_restaurants
 
 @cl.password_auth_callback
 def auth():
@@ -52,13 +44,9 @@ def auth_callback(username: str, password: str):
     
 
 @cl.oauth_callback
-def oauth_callback(
-  provider_id: str,
-  token: str,
-  raw_user_data: Dict[str, str],
-  default_user: cl.User,
-) -> Optional[cl.User]:
+def oauth_callback(provider_id: str, token: str, raw_user_data: Dict[str, str], default_user: cl.User) -> Optional[cl.User]:
   return default_user
+
 
 @cl.on_message
 async def main(message: cl.Message):
